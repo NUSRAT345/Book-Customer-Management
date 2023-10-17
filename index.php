9c38edda60624968da5f6e535ed5fe388ea20825
NUSRAT345 <81955383+NUSRAT345@users.noreply.github.com>
2023-10-17 12:14:04 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <html>
 <head>
     <title>Book and Customer</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0-beta2/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         .custom-card {
             border-radius: 5px;
